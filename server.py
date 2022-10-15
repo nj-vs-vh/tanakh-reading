def9e56d496f8db7e75a4ee0f3ca5d47e40cecf7
Igor Vaiman <gosha.vaiman@gmail.com>
2022-10-16 00:53:09 +0400

    correct CORS header specified

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ async def auth_middleware(request: web.Request, handler: Handler) -> web.StreamR
 @web.middleware
 async def cors_middleware(request: web.Request, handler: Handler) -> web.StreamResponse:
     resp = await handler(request)
-    allowed_origin = "https://ru-semantle.surge.sh" if config.IS_PROD else "http://localhost:8080"
+    allowed_origin = "https://torah-reading.surge.sh" if config.IS_PROD else "http://localhost:8080"
     resp.headers[hdrs.ACCESS_CONTROL_ALLOW_ORIGIN] = allowed_origin
     resp.headers[hdrs.ACCESS_CONTROL_ALLOW_HEADERS] = "Content-Type"
     resp.headers[hdrs.ACCESS_CONTROL_ALLOW_METHODS] = "POST, GET, OPTIONS"
