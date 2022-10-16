d5e37b2fbf247c824da49ffa0d85ba012d85df9b
Igor Vaiman <gosha.vaiman@gmail.com>
2022-10-16 21:36:27 +0400

    scrollable verse modals

--- frontend/src/components/VerseDetailsModal.svelte
+++ frontend/src/components/VerseDetailsModal.svelte
@@ -1,25 +1,121 @@
 <script lang="ts">
-    import { TextSource, VerseData } from "../types";
+    import Keydown from "svelte-keydown";
+    import { ParshaData, TextSource, VerseData } from "../types";
     import { commentSourceFlagsStore } from "../settings/commentSources";
     import type { CommentSourceFlags } from "../settings/commentSources";
-
-    import VerseComments from "./VerseComments.svelte"
+    import VerseComments from "./VerseComments.svelte";
+    import InlineIcon from "./shared/InlineIcon.svelte";
+    import Icon from "./shared/Icon.svelte";
 
     let commentSourceFlags: CommentSourceFlags;
     commentSourceFlagsStore.subscribe((v) => {
         commentSourceFlags = v;
     });
 
-    export let verseData: VerseData;
+    export let parsha: ParshaData;
+    export let verse: number;
     export let chapter: number;
+
+    interface VerseCoords {
+        chapter: number;
+        verse: number;
+    }
+
+    let currentVerseCoords: VerseCoords = { chapter: chapter, verse: verse };
+
+    function findVerseCoords(
+        chapterNo: number,
+        verseNo: number
+    ): VerseCoords | null {
+        const chapterData = parsha.chapters.find(
+            (ch) => ch.chapter === chapterNo
+        );
+        if (chapterData === undefined) return null;
+        let verseData: VerseData | null = null;
+        if (verseNo >= 0)
+            verseData = chapterData.verses.find((vd) => vd.verse === verseNo);
+        // support for Python-like negative verse index
+        else
+            verseData = chapterData.verses.find(
+                (vd) => vd.verse === chapterData.verses.length + verseNo + 1
+            );
+        if (verseData === undefined) return null;
+        else return { chapter: chapterData.chapter, verse: verseData.verse };
+    }
+
+    let prevVerseCoords: VerseCoords | null;
+    let nextVerseCoords: VerseCoords | null;
+    let currentVerseData: VerseData;
+
+    $: {
+        currentVerseData = parsha.chapters
+            .find((ch) => ch.chapter === currentVerseCoords.chapter)
+            .verses.find((v) => v.verse === currentVerseCoords.verse);
+
+        prevVerseCoords = findVerseCoords(
+            currentVerseCoords.chapter,
+            currentVerseCoords.verse - 1
+        );
+        if (prevVerseCoords === null)
+            prevVerseCoords = findVerseCoords(
+                currentVerseCoords.chapter - 1,
+                -1
+            );
+        nextVerseCoords = findVerseCoords(
+            currentVerseCoords.chapter,
+            currentVerseCoords.verse + 1
+        );
+        if (nextVerseCoords === null)
+            nextVerseCoords = findVerseCoords(
+                currentVerseCoords.chapter + 1,
+                1
+            );
+    }
+
+    function prevVerse() {
+        if (prevVerseCoords !== null) currentVerseCoords = prevVerseCoords;
+    }
+
+    function nextVerse() {
+        if (nextVerseCoords !== null) currentVerseCoords = nextVerseCoords;
+    }
 </script>
 
+<Keydown on:ArrowRight={nextVerse} on:ArrowLeft={prevVerse} />
 <div class="container">
-    <p class="verse-number">{chapter}:{verseData.verse}</p>
+    <p class="verse-nav">
+        <span
+            class="arrow-container"
+            on:click={(e) => prevVerse()}
+            on:keyup={(e) => {}}
+        >
+            <InlineIcon heightEm={0.8}>
+                <Icon
+                    icon="chevron-left"
+                    color={prevVerseCoords !== null ? "grey" : "white"}
+                />
+            </InlineIcon>
+        </span>
+        <span class="verse-number">
+            {currentVerseCoords.chapter}:{currentVerseData.verse}
+        </span>
+        <span
+            class="arrow-container"
+            on:click={(e) => nextVerse()}
+            on:keyup={(e) => {}}
+        >
+            <InlineIcon heightEm={0.8}>
+                <Icon
+                    icon="chevron-right"
+                    color={nextVerseCoords !== null ? "grey" : "white"}
+                />
+            </InlineIcon>
+        </span>
+    </p>
     <blockquote>
-        {verseData.text[TextSource.FG]}
+        {currentVerseData.text[TextSource.FG]}
     </blockquote>
-    <VerseComments {verseData}/>
+    <VerseComments verseData={currentVerseData} />
 </div>
 
 <style>
@@ -32,8 +128,28 @@
         margin: 0.2em 0 0.1em 0;
     }
 
-    p.verse-number {
+    p.verse-nav {
+        display: inline-flex;
+        align-items: center;
+    }
+
+    span.arrow-container {
+        display: flex;
+        align-items: baseline;
+        cursor: pointer;
+    }
+
+    span.arrow-container:hover {
+        background-image: radial-gradient(
+            closest-side,
+            rgb(221, 221, 221),
+            transparent
+        );
+    }
+
+    span.verse-number {
         color: grey;
+        margin: 0 0.5em;
     }
 
     blockquote {
