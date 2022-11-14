90fd4a4568589def17f318d9770df9a58d0d6725
Igor Vaiman <gosha.vaiman@gmail.com>
2022-11-14 22:45:24 +0400

    backend support for editing

--- parsers/upload.py
+++ parsers/upload.py
@@ -13,7 +13,7 @@ async def main(index: int):
     parsha_data = json.loads(parsha_path(index).read_text())
     async with aiohttp.ClientSession() as session:
         response = await session.post(
-            f"{os.environ['BASE_URL']}/parsha/{index}",
+            f"{os.environ['BASE_URL']}/parsha",
             json=parsha_data,
             headers={"X-Admin-Token": os.environ["ADMIN_TOKEN"]},
         )
