14bf355537c0526034acb60f5f8a06df8b8d2984
Igor Vaiman <gosha.vaiman@gmail.com>
2023-07-16 15:09:37 +0400

    fix

--- parsers/all.py
+++ parsers/all.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     PARSHA_URL_NAME = args.parsha_url_name_lechaim
 
     subprocess_args_list = [
-        # ["python", "parsers/shabat_shalom_parser.py", PARSHA],
-        # ["python", "parsers/reformjudaism_parser.py", PARSHA],
-        # ["python", "parsers/lechaim_parser.py", PARSHA, PARSHA_URL_NAME],
+        ["python", "parsers/shabat_shalom_parser.py", PARSHA],
+        ["python", "parsers/reformjudaism_parser.py", PARSHA],
+        ["python", "parsers/lechaim_parser.py", PARSHA, PARSHA_URL_NAME],
         ["python", "parsers/ramban_commentary_parser.py", PARSHA],
         ["python", "parsers/or_hachaim_commentary_parser.py", PARSHA],
         ["python", "parsers/hebrew_parser.py", PARSHA],
