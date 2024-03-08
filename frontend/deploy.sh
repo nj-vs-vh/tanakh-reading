bde7098e6db8c4e7b361ce13471876053a0e7861
Igor Vaiman <igor.vaiman@inten.to>
2024-03-08 19:09:38 +0100

    double frontend publishing added

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,3 +1,8 @@
 export GIT_COMMIT_ID=$(git describe --always)
 npm run build
+
+echo "Publishing to Surge"
 surge public torah-reading.surge.sh
+
+echo "Publishing to my website"
+rsync -azP public/ knor:tanakh-reading/frontend/public/
