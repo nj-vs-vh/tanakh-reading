f7a6b62e8ec5a14ffa068635fbacbfbca232fbbf
Igor Vaiman <igor.vaiman@inten.to>
2024-03-08 18:47:45 +0100

    new backend

--- backend/server.py
+++ backend/server.py
@@ -55,6 +55,8 @@ async def cors_middleware(request: web.Request, handler: Handler) -> web.StreamR
     allowed_origins = [
         "https://torah-reading.surge.sh",
         "http://torah-reading.surge.sh",
+        "https://tanakh-reading.nj-vs-vh.name",
+        "http://tanakh-reading.nj-vs-vh.name",
         "http://localhost:8080",
     ]
     request_origin = request.headers.get(hdrs.ORIGIN)
